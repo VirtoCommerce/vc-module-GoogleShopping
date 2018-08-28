bd34db5ee4c880106bb2e617b893dfe1f51dde72
Andrey A. Vasenev <avas.the.wise@gmail.com>
2018-08-28 13:32:00 +0700

    Moved hard-coded credential data to Web.config.

---
+++ GoogleShopping.Merchant.Web/Providers/ConfigBasedGoogleMerchantCenterCredentialsProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.ShoppingContent.v2;
+using VirtoCommerce.Platform.Core.Common;
+
+namespace GoogleShopping.MerchantModule.Web.Providers
+{
+    public class ConfigBasedGoogleMerchantCenterCredentialsProvider : IGoogleMerchantCenterCredentialsProvider
+    {
+        private readonly string _serviceAccountEmailPropertyName;
+        private readonly string _certificatePathPropertyName;
+        private readonly string _certificatePassphrasePropertyName;
+
+        public ConfigBasedGoogleMerchantCenterCredentialsProvider(string serviceAccountEmailPropertyName, 
+            string certificatePathPropertyName, string certificatePassphrasePropertyName)
+        {
+            _serviceAccountEmailPropertyName = serviceAccountEmailPropertyName;
+            _certificatePathPropertyName = certificatePathPropertyName;
+            _certificatePassphrasePropertyName = certificatePassphrasePropertyName;
+        }
+
+        public ServiceAccountCredential GetCredential()
+        {
+            var serviceAccountEmail = ConfigurationHelper.GetAppSettingsValue(_serviceAccountEmailPropertyName);
+            var certificatePath = ConfigurationHelper.GetAppSettingsValue(_certificatePathPropertyName);
+            var certificatePassphrase = ConfigurationHelper.GetAppSettingsValue(_certificatePassphrasePropertyName);
+
+            var certificate = new X509Certificate2(certificatePath, certificatePassphrase, X509KeyStorageFlags.Exportable);
+
+            var credentialInitializer = new ServiceAccountCredential.Initializer(serviceAccountEmail)
+                {
+                    Scopes = new[] { ShoppingContentService.Scope.Content }
+                }.FromCertificate(certificate);
+
+            return new ServiceAccountCredential(credentialInitializer);
+        }
+    }
+}
--- GoogleShopping.Merchant.Web/Providers/ServiceGoogleContentServiceProvider.cs
+++ GoogleShopping.Merchant.Web/Providers/ServiceGoogleContentServiceProvider.cs
@@ -1,32 +1,24 @@
-﻿using System.IO;
-using System.Security.Cryptography.X509Certificates;
-using System.Web.Hosting;
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Services;
+﻿using Google.Apis.Services;
 using Google.Apis.ShoppingContent.v2;
 
 namespace GoogleShopping.MerchantModule.Web.Providers
 {
     public class ServiceGoogleContentServiceProvider : IGoogleContentServiceProvider
     {
-        private readonly string serviceAccountEmail = "39718569872-p1gucbblanda96o6nr9bbrjdekv8euba@developer.gserviceaccount.com";
-        const string keyPath = @"App_Data\Modules\key.p12";
+        private readonly IGoogleMerchantCenterCredentialsProvider _credentialsProvider;
+
         private ShoppingContentService _contentService;
+
+        public ServiceGoogleContentServiceProvider(IGoogleMerchantCenterCredentialsProvider credentialsProvider)
+        {
+            _credentialsProvider = credentialsProvider;
+        }
 
         public ShoppingContentService GetShoppingContentService()
         {
             if (_contentService == null)
             {
-                string appPath = HostingEnvironment.ApplicationPhysicalPath;
-                var key = Path.Combine(appPath, keyPath);
-
-                var certificate = new X509Certificate2(key, "notasecret", X509KeyStorageFlags.Exportable);
-
-                var credential = new ServiceAccountCredential(
-                    new ServiceAccountCredential.Initializer(serviceAccountEmail)
-                    {
-                        Scopes = new[] { ShoppingContentService.Scope.Content }
-                    }.FromCertificate(certificate));
+                var credential = _credentialsProvider.GetCredential();
 
                 // Create the service.
                 _contentService = new ShoppingContentService(new BaseClientService.Initializer()
